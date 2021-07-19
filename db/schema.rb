9f7a688ff6fec24d342f80a58dc5c9e0b03b4c6a
yusei.ohara <yusei20380178@icloud.com>
2021-07-19 02:06:37 +0000

    DRY

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_16_050704) do
+ActiveRecord::Schema.define(version: 2021_07_18_091534) do
 
   create_table "tasks", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "status"
   end
 
 end
